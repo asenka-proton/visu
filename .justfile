d2c6a4b962ac524acde60df298a5997b6ee014a3
asenka-proton <asenka.proton@pm.me>
2026-07-28 09:29:40 +0200

    feat: layout engine

--- .justfile
+++ .justfile
@@ -17,3 +17,14 @@ test:
 setup:
     gradle wrapper
 
+bundle-sources output-file="project-visu-sources.txt":
+    find . -type f \( -name "*.java" -o -name "*.kts" -o -name "*.properties" \) \
+        ! -path "*/target/*" \
+        ! -path "*/build/*" \
+        ! -path "*/.git/*" \
+        ! -path "*/.idea/*" \
+        ! -path "*/gradle/*" \
+        ! -path "*/.gradle/*" \
+        ! -path "*/.run/*" \
+        | xargs -I {} sh -c 'echo "--- FILE: {} ---"; cat {}; echo ""' > {{ output-file }}
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("java")
     id("application")
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 application {
@@ -9,7 +11,7 @@ application {
 }
 
 javafx {
-    version = "21"
+    version = "${project.property("version-javafx")}"
     modules("javafx.controls", "javafx.graphics", "javafx.fxml")
 }
 
@@ -22,17 +24,19 @@ tasks.test {
 }
 
 dependencies {
-    implementation("org.openjfx:javafx-controls:21.0.2")
-    implementation("org.openjfx:javafx-graphics:21.0.2")
-    implementation("org.openjfx:javafx-fxml:21.0.2")
 
-    compileOnly("org.projectlombok:lombok:${project.property("version-lombok")}")
+    implementation("org.springframework.boot:spring-boot-starter:${project.property("version-spring-boot")}")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:${project.property("version-junit")}")
+    implementation("org.openjfx:javafx-controls:${project.property("version-javafx")}")
+    implementation("org.openjfx:javafx-graphics:${project.property("version-javafx")}")
+    implementation("org.openjfx:javafx-fxml:${project.property("version-javafx")}")
+
     testImplementation("org.assertj:assertj-core:${project.property("version-assertj")}")
 
+    testImplementation("org.junit.jupiter:junit-jupiter:${project.property("version-junit")}")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:${project.property("version-junit")}")
 
+    compileOnly("org.projectlombok:lombok:${project.property("version-lombok")}")
     annotationProcessor("org.projectlombok:lombok:${project.property("version-lombok")}")
 }
 
